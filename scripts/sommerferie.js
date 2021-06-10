7ac25fe992177be0ca81fb163fa798ebf147aa71
soni801 <32430506+soni801@users.noreply.github.com>
2021-06-10 13:10:01 +0200

    Improve JS comments

--- scripts/sommerferie.js
+++ scripts/sommerferie.js
@@ -1,26 +1,26 @@
-// Setter klokkelsett og dato som vi teller ned til
+// Sett tidspunkt som vi teller ned til
 var countDownDate = new Date("Jun 18, 2021 12:00:00").getTime();
 
-// Oppdaterer nedtellingen hvert sekund
+// Oppdater nedtellingen hvert sekund
 var x = setInterval(function() {
 
-    // Henter dagent dato og klokkeslett
+    // Hent nåværende tidspunkt
     var now = new Date().getTime();
 
-    // Finner avstanden mellom dagens dato og klokkelsett og den vi teller ned til
+    // Finn tid fra nåværende tidspunkt til satt tidspunkt
     var distance = countDownDate - now;
 
-    // kalkuleringen av dager, timer, minutter og sekunder
+    // Regn ut tidselementer
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Viser resultatet i elementet id=demo
+    // Vis resultat i element med ID 'nedtelling'
     document.getElementById("nedtelling").innerHTML = "Antall dager til sommerferien: "+ days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
 
-    // Når nedtellingen er gjennomført, så vis følgende tekst
+    // Vis 'EXPIRED' om resultat er negativt
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("nedtelling").innerHTML = "EXPIRED";
